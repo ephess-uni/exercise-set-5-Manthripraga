d4d00414ec82319c22d3df6c3563ac518e1da855
Manthripraga <136190794+Manthripraga@users.noreply.github.com>
2023-07-21 16:44:43 -0500

    Update ex_5_4.py
    complette

--- src/ex_5_4.py
+++ src/ex_5_4.py
@@ -1,3 +1,4 @@
+
 """ex_5_4.py"""
 import numpy as np
 from pathlib import Path
@@ -15,6 +16,9 @@ data_dir = root_dir / "data"
 output_dir = root_dir / "outputs"
 input_file = data_dir / "ex_5_4-data.csv"
 output_file = output_dir / "ex_5_4-processed.csv"
+input_data = np.loadtxt(input_file)
+input_data[input_data<0] = 0
+np.savetxt(output_file, input_data)
 
 # Process the input data using numpy
 
